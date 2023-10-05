3df314beab3a1134ec327670c75d16a0ae63df74
Ondřej Knopp <knopp@karel.troja.mff.cuni.cz>
2023-10-05 21:14:31 +0200

    trim geoloc

--- src/routes/api/nearme/+server.ts
+++ src/routes/api/nearme/+server.ts
@@ -5,8 +5,8 @@ import type { RequestHandler } from "@sveltejs/kit";
 export const GET:RequestHandler=(async ({url, setHeaders}) => {
     setHeaders({"cache-control":"max-age=24*60*60"})
     let closest = stops[0];
-    const lat = Number(url.searchParams.get('lat'))
-    const lon = Number(url.searchParams.get('lon'))
+    const lat = Number(url.searchParams.get('lat')?.slice(0,7))
+    const lon = Number(url.searchParams.get('lon')?.slice(0,7))
     const scale = Math.cos(Math.PI*lat/180.0)
     stops.forEach((stop) => {
         const dist =(stop.lat-lat)*(stop.lat-lat)+scale*scale*(stop.lon-lon)*(stop.lon-lon);
